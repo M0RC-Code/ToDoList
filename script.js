da8885eef0bc117737abe02f85a7fa420ddac2c9
Mardin <M0RC.DevOrg@gmail.com>
2026-07-07 23:50:20 +0330

    create btn delete and complete for all task

--- script.js
+++ script.js
@@ -3,10 +3,24 @@ const inputEl = document.querySelector(".input-text");
 const inputBtnEl = document.querySelector(".input-task-btn");
 const listEl = document.querySelector(".list");
 const listItemEl = document.querySelector(".list-item");
+const deleteTaskBtn = document.querySelector(".delete-task-btn");
+const completeTaskBtn = document.querySelector(".complete-task-btn");
 
 // add new task to list
 inputBtnEl.addEventListener("click", () => {
+    let newTask = inputEl.value;
     listEl.insertAdjacentHTML("beforeend", `
-        <li class="list-item">${inputEl.value}</li>`);
+        <li class="list-item">${newTask}</li>`);
     inputEl.value = "";
+    listEl.classList.remove("complete");
+})
+
+// delete all task
+deleteTaskBtn.addEventListener("click", () => {
+    listEl.innerHTML = "";
+})
+
+// complete all task
+completeTaskBtn.addEventListener("click", () => {
+    listEl.classList.add("complete");
 })
