e3a9ea88f337c3c351ddd2413848624e5fe0f144
Mardin <M0RC.DevOrg@gmail.com>
2026-07-07 23:23:06 +0330

    create and set element

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select element from DOM
+const inputEl = document.querySelector(".input-text");
+const inputBtnEl = document.querySelector(".input-task-btn");
+const listEl = document.querySelector(".list");
+const listItemEl = document.querySelector(".list-item");
+
+// add new task to list
