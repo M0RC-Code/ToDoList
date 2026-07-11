40f86c1f8e5fa7a30005701a9eef9e7af0b2e65f
Mardin <M0RC.DevOrg@gmail.com>
2026-07-11 21:54:56 +0330

    sent task to local storage and save task and reload task

--- script.js
+++ script.js
@@ -9,14 +9,40 @@ const taskEl = document.querySelector(".task")
 const deleteTaskEl = document.querySelector(".delete-task-btn");
 const completeTaskEl = document.querySelector(".complete-task-btn");
 
+// create tasks object
+let tasks = [{titleTask: "hello", completed: false}];
+
+function loadTask(){
+    let loadTask = localStorage.getItem("tasks");
+    tasks = JSON.parse(loadTask);
+    tasks.forEach((task) => {
+        listEl.insertAdjacentHTML("beforeend", `
+            <div class="task">
+                <li class="list-item">${task.titleTask}</li>
+                <div class="task-btn-delete-complete">
+                    <button class="delete-task-btn btn-list btn">delete</button>
+                    <button class="complete-task-btn btn-list btn">complete</button>
+                </div>
+            </div>`);
+    })
+}
+loadTask();
+// add new task func
 function addNewTask() {
-    task = [];
-    let newTask = inputEl.value.trim();
-    if (newTask === "") return;
-    task.push(newTask);
+    let titleTask;
+    let newTaskTitle = inputEl.value.trim();
+    if (newTaskTitle === "") return;
+    titleTask = newTaskTitle;
+    
+    // add new task to tasks object
+    tasks.push({
+        titleTask: titleTask,
+        completed: false
+    })
+    // Create new task to page
     listEl.insertAdjacentHTML("beforeend", `
             <div class="task">
-                <li class="list-item">${task}</li>
+                <li class="list-item">${titleTask}</li>
                 <div class="task-btn-delete-complete">
                     <button class="delete-task-btn btn-list btn">delete</button>
                     <button class="complete-task-btn btn-list btn">complete</button>
@@ -25,8 +51,15 @@ function addNewTask() {
 
     inputEl.value = "";
     listEl.classList.remove("complete");
-}
 
+    console.log(tasks)
+    // save new task
+    saveTask()
+}
+// save task to local storage
+function saveTask(){
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
 // add new task to list
 inputBtnEl.addEventListener("click", addNewTask)
 // add event delete and complete
@@ -67,3 +100,5 @@ completeAllTaskBtn.addEventListener("click", () => {
         task.classList.add("complete");
     });
 });
+
+// localStorage
